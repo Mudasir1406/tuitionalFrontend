7c9299c73f3f495bdabc963765c8864cdfa2e3ed
Mudasir1406 <mudasir1406@gmail.com>
2024-09-04 14:03:54 +0500

    typescript fix

--- src/components/grade-subject-level/Questions.tsx
+++ src/components/grade-subject-level/Questions.tsx
@@ -1,10 +1,10 @@
-"use client"
-import { Box, Grid, Typography, Collapse } from '@mui/material';
-import React, { useState } from 'react';
-import upicon from '../../../public/assets/images/svg/Upicon.svg';
-import downicon from '../../../public/assets/images/static/Downicon.png';
-import Image from 'next/image';
-import type { StaticImageData } from 'next/image';
+"use client";
+import { Box, Grid, Typography, Collapse } from "@mui/material";
+import React, { useState } from "react";
+import upicon from "../../../public/assets/images/svg/Upicon.svg";
+import downicon from "../../../public/assets/images/static/Downicon.png";
+import Image from "next/image";
+import type { StaticImageData } from "next/image";
 
 interface Question {
   question: string;
@@ -12,48 +12,11 @@ interface Question {
   icon: StaticImageData;
 }
 
-const FrequentlyQuestions: React.FC = ({ data }: any) => {
-  console.log(data?.faqs)
+type IProps = {
+  data: any;
+};
+const FrequentlyQuestions: React.FC<IProps> = ({ data }: any) => {
   const [expanded, setExpanded] = useState<number>(0);
-
-  const Questions: Question[] = [
-    {
-      question: "Lorem ipsum dolor sit amet consectetur. Lorem leo felis.",
-      answer: "Lorem ipsum dolor sit amet consectetur. Nunc malesuada massa enim nec sapien vel sagittis dignissim libero. Felis phasellus cursus dolor suspendisse. Quam enim urna dictumst aenean morbi nisi. Molestie tincidunt id neque mauris. Egestas nisi tellus eget id aenean dignissim turpis risus. Nisi felis.",
-      icon: downicon,
-    },
-    {
-      question: "Lorem ipsum dolor sit amet consectetur. Faucibus.",
-      answer: "Excellent Answer",
-      icon: downicon,
-    },
-    {
-      question: "Lorem ipsum dolor sit amet consectetur. Ultricies nibh.",
-      answer: "Excellent Answer",
-      icon: downicon,
-    },
-    {
-      question: "Lorem ipsum dolor sit amet consectetur. Quis in rhoncus id libero molestie sed.",
-      answer: "Excellent Answer",
-      icon: downicon,
-    },
-    {
-      question: "Lorem ipsum dolor sit amet consectetur. Pulvinar accumsan dui malesuada commodo euismod.",
-      answer: "Excellent Answer",
-      icon: downicon,
-    },
-    {
-      question: "Lorem ipsum dolor sit amet consectetur. Purus quis venenatis eget.",
-      answer: "Excellent Answer",
-      icon: downicon,
-    },
-    {
-      question: "Lorem ipsum dolor sit amet consectetur. A tincidunt at morbi.",
-      answer: "Excellent Answer",
-      icon: downicon,
-    },
-  ];
-
   const handleToggle = (index: number) => {
     setExpanded(expanded === index ? 0 : index);
   };
@@ -61,43 +24,92 @@ const FrequentlyQuestions: React.FC = ({ data }: any) => {
   return (
     <Box sx={{ marginY: { lg: "13vh" }, marginX: { lg: "4vh" } }}>
       <Box>
-        <Typography sx={style.frequently}>Frequently Asked Questions</Typography>
-        <Typography sx={style.frequentlyDesc}>
-          {data?.paragraph}
+        <Typography sx={style.frequently}>
+          Frequently Asked Questions
         </Typography>
+        <Typography sx={style.frequentlyDesc}>{data?.paragraph}</Typography>
       </Box>
 
       <Box sx={{ marginX: "auto", maxWidth: "140vh", paddingX: { lg: "2vh" } }}>
         <Grid container spacing={1}>
-          {data?.faqs.map((item: { question: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; answer: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; }, index: React.Key | null | undefined) => (
-            <Grid item xs={12} sm={12} md={12} lg={12} key={index}>
-              <Box
-                sx={{
-                  borderRadius: "2vh",
-                  border: "0.784px #EBEBEB",
-                  background: expanded === index ? "#9EDCFF" : "#F3FBFF", // Change background for active item
-                  backdropFilter: "blur(5px)",
-                  padding: "3vh",
-                  marginTop: "3vh",
-                  width: "100%", // Ensure consistent width
-                  boxSizing: "border-box", // Include padding and border in width
-                  minWidth: "300px", // Set a minimum width for consistency
-                }}
-              >
-                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                  <Typography sx={style.boxhed}>{item.question}</Typography>
-                  <Box onClick={() => handleToggle(index)} sx={{ cursor: "pointer" }}>
-                    <Image src={expanded === index ? upicon : downicon} alt="toggle icon" style={{ height: "4vh", width: "4vh" }} />
+          {data?.faqs.map(
+            (
+              item: {
+                question:
+                  | string
+                  | number
+                  | bigint
+                  | boolean
+                  | React.ReactElement<
+                      any,
+                      string | React.JSXElementConstructor<any>
+                    >
+                  | Iterable<React.ReactNode>
+                  | React.ReactPortal
+                  | Promise<React.AwaitedReactNode>
+                  | null
+                  | undefined;
+                answer:
+                  | string
+                  | number
+                  | bigint
+                  | boolean
+                  | React.ReactElement<
+                      any,
+                      string | React.JSXElementConstructor<any>
+                    >
+                  | Iterable<React.ReactNode>
+                  | React.ReactPortal
+                  | Promise<React.AwaitedReactNode>
+                  | null
+                  | undefined;
+              },
+              index: React.Key | null | undefined
+            ) => (
+              <Grid item xs={12} sm={12} md={12} lg={12} key={index}>
+                <Box
+                  sx={{
+                    borderRadius: "2vh",
+                    border: "0.784px #EBEBEB",
+                    background: expanded === index ? "#9EDCFF" : "#F3FBFF", // Change background for active item
+                    backdropFilter: "blur(5px)",
+                    padding: "3vh",
+                    marginTop: "3vh",
+                    width: "100%", // Ensure consistent width
+                    boxSizing: "border-box", // Include padding and border in width
+                    minWidth: "300px", // Set a minimum width for consistency
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography sx={style.boxhed}>{item.question}</Typography>
+                    <Box
+                      onClick={() => handleToggle(Number(index))}
+                      sx={{ cursor: "pointer" }}
+                    >
+                      <Image
+                        src={expanded === index ? upicon : downicon}
+                        alt="toggle icon"
+                        style={{ height: "4vh", width: "4vh" }}
+                      />
+                    </Box>
                   </Box>
+                  <Collapse
+                    in={expanded === index}
+                    timeout="auto"
+                    unmountOnExit
+                  >
+                    <Typography sx={style.boxdesc}>{item.answer}</Typography>
+                  </Collapse>
                 </Box>
-                <Collapse in={expanded === index} timeout="auto" unmountOnExit>
-                  <Typography sx={style.boxdesc}>
-                    {item.answer}
-                  </Typography>
-                </Collapse>
-              </Box>
-            </Grid>
-          ))}
+              </Grid>
+            )
+          )}
         </Grid>
       </Box>
     </Box>
--- src/components/grade-subject-level/Tutoring-options.tsx
+++ src/components/grade-subject-level/Tutoring-options.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Grid, Theme, Typography } from '@mui/material';
-import { SystemStyleObject } from '@mui/system';
-import { ResponsiveStyleValue } from '@mui/system/styleFunctionSx';
-import { Property } from 'csstype';
-import React from 'react';
+import { Box, Button, Grid, Theme, Typography } from "@mui/material";
+import { SystemStyleObject } from "@mui/system";
+import { ResponsiveStyleValue } from "@mui/system/styleFunctionSx";
+import { Property } from "csstype";
+import React from "react";
 
 interface SubjectArray {
   SubjectsArray: string;
@@ -12,52 +12,104 @@ interface SubjectArray {
 interface SubjectsArrayProps {
   Header: any;
   Paragraph: string;
-  SubjectsArray: any
+  SubjectsArray: any;
 }
 
-const TutoringOptions: React.FC<SubjectsArrayProps> = ({ SubjectsArray = [], Paragraph, Header }) => {
-
+const TutoringOptions: React.FC<SubjectsArrayProps> = ({
+  SubjectsArray = [],
+  Paragraph,
+  Header,
+}) => {
   return (
     <>
       <Box sx={{ margin: "7vh" }}>
-        <Typography sx={style.title}>
-          {Header}
-        </Typography>
-        <Typography sx={style.description}>
-          {Paragraph}
-        </Typography>
+        <Typography sx={style.title}>{Header}</Typography>
+        <Typography sx={style.description}>{Paragraph}</Typography>
 
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column', // Stack the rows vertically
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column", // Stack the rows vertically
           }}
         >
-          <Grid container spacing={2} sx={{ maxWidth: '170vh', margin: "4vh auto" }}>
-            {SubjectsArray.map((button: { backgroundColor: (string & {}) | SystemStyleObject<Theme> | (string | number) | ((theme: Theme) => string | number | SystemStyleObject<Theme>) | readonly string[] | readonly (readonly string[] | Property.BackgroundColor | null | undefined)[] | { [key: string]: readonly string[] | Property.BackgroundColor | null | undefined; } | ((theme: Theme) => ResponsiveStyleValue<readonly string[] | Property.BackgroundColor | undefined>) | undefined; name: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; }, index: React.Key | null | undefined) => (
-              <Grid item xs={12} sm={6} key={index}>
-                <Button
-                  sx={{
-                    backgroundColor: index === 0 ? button.backgroundColor : '#FFFFFF',
-                    color: '#2D2D2D', // Text color
-                    width: '100%',
-                    borderRadius: index === 0 ? '5vh' : '50px', // Apply border-radius conditionally
-                    padding: '16px',
-                    textAlign: 'center',
-                    // boxShadow: index > 0 ? '0px -1px 10px 0px rgba(0, 0, 0, 0.15)' : 'none', //
-                    fontSize: "2vh",
-                    fontWeight: 500,
-                  }}
-                >
-                  {button.name}
-                </Button>
-              </Grid>
-            ))}
+          <Grid
+            container
+            spacing={2}
+            sx={{ maxWidth: "170vh", margin: "4vh auto" }}
+          >
+            {SubjectsArray.map(
+              (
+                button: {
+                  backgroundColor:
+                    | (string & {})
+                    | SystemStyleObject<Theme>
+                    | (string | number)
+                    | ((
+                        theme: Theme
+                      ) => string | number | SystemStyleObject<Theme>)
+                    | readonly string[]
+                    | readonly (
+                        | readonly string[]
+                        | Property.BackgroundColor
+                        | null
+                        | undefined
+                      )[]
+                    | {
+                        [key: string]:
+                          | readonly string[]
+                          | Property.BackgroundColor
+                          | null
+                          | undefined;
+                      }
+                    | ((
+                        theme: Theme
+                      ) => ResponsiveStyleValue<
+                        readonly string[] | Property.BackgroundColor | undefined
+                      >)
+                    | undefined;
+                  name:
+                    | string
+                    | number
+                    | bigint
+                    | boolean
+                    | React.ReactElement<
+                        any,
+                        string | React.JSXElementConstructor<any>
+                      >
+                    | Iterable<React.ReactNode>
+                    | React.ReactPortal
+                    | Promise<React.AwaitedReactNode>
+                    | null
+                    | undefined;
+                },
+                index: React.Key | null | undefined
+              ) => (
+                <Grid item xs={12} sm={6} key={index}>
+                  <Button
+                    sx={{
+                      backgroundColor:
+                        index === 0
+                          ? String(button.backgroundColor)
+                          : "#FFFFFF",
+                      color: "#2D2D2D", // Text color
+                      width: "100%",
+                      borderRadius: index === 0 ? "5vh" : "50px", // Apply border-radius conditionally
+                      padding: "16px",
+                      textAlign: "center",
+                      fontSize: "2vh",
+                      fontWeight: 500,
+                    }}
+                  >
+                    {button.name}
+                  </Button>
+                </Grid>
+              )
+            )}
           </Grid>
         </Box>
-      </Box >
+      </Box>
     </>
   );
 };
@@ -77,11 +129,11 @@ const style = {
   description: {
     color: "#2D2D2D",
     width: {
-      lg: "190vh"
+      lg: "190vh",
     },
     fontSize: {
       lg: "2vh",
     },
     fontWeight: 400,
-  }
-}
+  },
+};
