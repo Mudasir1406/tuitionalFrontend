91fa62e2aeac19e52750523c6cea789d7398a38c
Mudasir1406 <mudasir1406@gmail.com>
2024-10-08 15:47:56 +0500

    ui

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,6 +2,5 @@ import { League_Spartan } from "next/font/google";
 
 export const leagueSpartan = League_Spartan({
   subsets: ["latin"],
-  variable: "--font-leagueSpartan",
   display: "swap",
 });
