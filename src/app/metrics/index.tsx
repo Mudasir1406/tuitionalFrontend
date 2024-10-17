7784cc0ee3191798e6e76d7f589ce87a9dbc4252
Mudasir1406 <mudasir1406@gmail.com>
2024-10-17 15:15:09 +0500

    google analytics

--- src/app/metrics/index.tsx
+++ src/app/metrics/index.tsx
@@ -1,8 +1,10 @@
+import GoogleAnalytics from "./google-analytics";
 import MicrosoftClarity from "./microsoft-clarity";
 
 const Metrics = () => (
   <>
     <MicrosoftClarity />
+    <GoogleAnalytics />
   </>
 );
 
