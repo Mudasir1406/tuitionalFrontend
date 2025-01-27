b72bfa7dcd1b5492aa10cca50022bea40f149f49
Muhammad Ashir Khan <aaashir128@gmail.com>
2025-01-27 15:31:50 +0500

    footer link

--- src/components/footerLinks/FooterLinks.tsx
+++ src/components/footerLinks/FooterLinks.tsx
@@ -1,32 +1,34 @@
 "use client";
 
-import Link from "next/link";
-import { Typography } from "@mui/material"; // Adjust based on your imports
+import { Typography } from "@mui/material";
 import { leagueSpartan } from "@/app/fonts";
 import { findExactSubjectURL } from "@/utils/helper";
+import { useRouter } from "next/navigation";
 
 const FooterLinks = ({ footerData }: { footerData: any }) => {
+  const router = useRouter();
   return (
     <>
       {footerData?.slice(0, 10).map((item: string, index: number) => {
         const href = findExactSubjectURL(item);
         return (
-          <Link
-            href={href}
-            key={index}
-            style={{
-              textDecoration: "none",
-              color: "inherit",
-            }}
+          // <Link
+          //   href={href}
+          //   key={index}
+          //   style={{
+          //     textDecoration: "none",
+          //     color: "inherit",
+          //   }}
+          // >
+          <Typography
+            onClick={() => router.push(href)}
+            sx={styles.text}
+            variant="body2"
+            className={leagueSpartan.className}
           >
-            <Typography
-              sx={styles.text}
-              variant="body2"
-              className={leagueSpartan.className}
-            >
-              {item}
-            </Typography>
-          </Link>
+            {item}
+          </Typography>
+          // </Link>
         );
       })}
     </>
@@ -37,6 +39,7 @@ export default FooterLinks;
 
 const styles = {
   text: {
+    cursor: "pointer",
     lineHeight: {
       xs: "35px",
       sm: "40px",
