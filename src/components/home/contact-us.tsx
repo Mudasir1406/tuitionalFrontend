eed43efb618455bbb77a8a90ca0c881ea7a1fea2
Muhammad Ashir Khan <aaashir128@gmail.com>
2025-04-26 01:32:07 +0500

    contact img

--- src/components/home/contact-us.tsx
+++ src/components/home/contact-us.tsx
@@ -279,6 +279,9 @@ const ContactUs: React.FunctionComponent<IProps> = ({
               height={girl.height}
               alt="girl"
               className="girlGrid"
+              // fill
+              priority
+              quality={80}
               style={{
                 position: "absolute",
               }}
