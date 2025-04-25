6bb9888076e3e2b71bf0507d9ba6b66387e32c29
Muhammad Ashir Khan <aaashir128@gmail.com>
2025-04-25 23:39:06 +0500

    font.ts > diplay optional

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,5 +2,7 @@ import { League_Spartan } from "next/font/google";
 
 export const leagueSpartan = League_Spartan({
   subsets: ["latin"],
-  display: "swap",
+  display: "optional",
+  variable: "--font-league-spartan", // Add this line
+  adjustFontFallback: false, // Recommended for modern browsers
 });
