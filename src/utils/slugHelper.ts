2185bcb4cf9841d1aba4de943067ba7a1888ebfb
Muhammad Ashir Khan <aaashir128@gmail.com>
2025-05-02 20:10:07 +0500

    home page > get location in helper

--- src/utils/slugHelper.ts
+++ src/utils/slugHelper.ts
@@ -12,6 +12,13 @@ type GeoLocationData = {
   error: string | null;
 };
 
+const getLocation = async () => {
+  const res = await fetch("/api/location");
+  const data = await res.json();
+  return data;
+  //   console.log("User Location:", data);
+};
+
 const useGeoLocation = (): GeoLocationData => {
   const [geoData, setGeoData] = useState<GeoLocationData>({
     ip: null,
@@ -43,12 +50,15 @@ const useGeoLocation = (): GeoLocationData => {
         const { ip } = await ipResponse.json();
 
         // Get geo data
-        const geoResponse = await fetch(`https://ipapi.co/${ip}/json/`);
-        const geoData = await geoResponse.json();
+        // const geoResponse = await fetch(`https://ipapi.co/${ip}/json/`);
+        // const geoData = await geoResponse.json();
+        // const res = await fetch("/api/location");
+        // const geoData = await res.json();
+        const geoData: any = await getLocation();
 
         setGeoData({
           ip,
-          country: geoData.country_name,
+          country: geoData.country || null,
           browser,
           pageURL,
           date: currentDate,
