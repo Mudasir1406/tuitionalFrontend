b647911a40474a6a30fb06c671cd39edf3dd81f1
Mudasir1406 <mudasir1406@gmail.com>
2025-07-23 15:55:11 +0500

    work

--- src/app/metrics/index.tsx
+++ src/app/metrics/index.tsx
@@ -1,12 +1,14 @@
 import GoogleAnalytics from "./google-analytics";
 import GoogleTagManager from "./google-tag-manager";
 import MicrosoftClarity from "./microsoft-clarity";
+import Pixel from "./pixel";
 
 const Metrics = () => (
   <>
     <MicrosoftClarity />
     <GoogleAnalytics />
     <GoogleTagManager />
+    <Pixel />
   </>
 );
 
