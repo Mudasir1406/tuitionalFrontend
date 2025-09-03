da39b789567d06c683ed8104d5e186997116c517
aaashir128 <aaashir128@gmail.com>
2025-09-03 18:26:54 +0500

    translation

--- src/components/home/get-started.tsx
+++ src/components/home/get-started.tsx
@@ -49,7 +49,7 @@ const GetStarted: React.FunctionComponent<IProps> = ({ data }) => {
         <Grid container>
           {data?.map((item, index) => (
             <Grid item xs={12} lg={4} md={6} sm={12} key={index}>
-              <GetStartedBox {...item} />
+              <GetStartedBox {...item as any} />
             </Grid>
           ))}
         </Grid>
@@ -73,7 +73,7 @@ const GetStarted: React.FunctionComponent<IProps> = ({ data }) => {
         >
           {data?.map((item, index) => (
             <SwiperSlide key={index} style={styles.slide}>
-              <GetStartedBox {...item} />
+              <GetStartedBox {...item as any} />
             </SwiperSlide>
           ))}
         </Swiper>
